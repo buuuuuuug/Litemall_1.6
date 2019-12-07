f0ea3415704efa993cdfac08c15a640b66a7d97c
buuuuuuug <44496732+buuuuuuug@users.noreply.github.com>
2019-12-07 11:20:34 +0800

    first commit

---
+++ WxPayment/src/main/java/com/oolitemall/wxpayment/WxpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.oolitemall.wxpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxpaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxpaymentApplication.class, args);
+    }
+
+}
---
+++ WxPayment/src/test/java/com/oolitemall/wxpayment/WxpaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oolitemall.wxpayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxpaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
